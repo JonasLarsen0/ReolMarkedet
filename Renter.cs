f3551391a6667e5490dafd6767b5b8fe7042d3f5
Mathias Holmgaard <maho54059@edu.ucl.dk>
2023-09-25 15:22:43 +0200

    Add project files.

---
+++ Renter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReolMarkedet
+{
+    public class Renter
+    {
+        renterID int;
+            renterName string;
+            renterEmail string;
+            renterPhone int;
+            renterBankAccountDetails int;
+
+    }
+}
